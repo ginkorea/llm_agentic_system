6c5afd7ad7375ddc833eeb4b692ba35fd4925ff6
gompert <joshua.gompert@gmail.com>
2024-10-23 20:53:45 +0900

    added comments to main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from agents.base import Agent
 
 if __name__ == "__main__":
 
-    agent = Agent(brain_type='cognitive') # defaults to embedded memory, but can be changed to 'cuda' or 'openvino' for acceleration
+    agent = Agent(brain_type='simple') # defaults to embedded memory, but can be changed to 'cuda' or 'openvino' for acceleration
     agent.run()
 
-
+## to change the brain_type use simple or cognitive
+## to change the memory_type use embedded, cuda or openvino
 
